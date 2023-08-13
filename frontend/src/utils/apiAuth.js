541f65bb7bb27239b6a56dbe9d4f95a51ded2980
Anatoliy Oleynikov <oleinikovav.ao@gmail.com>
2023-08-13 19:30:36 +0300

    deploy

--- frontend/src/utils/apiAuth.js
+++ frontend/src/utils/apiAuth.js
@@ -13,6 +13,7 @@ export function register(data) {
   return fetch(`${BASE_URL}/signup`, {
     method: 'POST',
     headers: {
+      "Access-Control-Allow-Origin": "*",
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
@@ -24,6 +25,7 @@ export function login(data) {
   return fetch(`${BASE_URL}/signin`, {
     method: 'POST',
     headers: {
+      "Access-Control-Allow-Origin": "*",
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
@@ -34,6 +36,7 @@ export function checkToken(token) {
   return fetch(`${BASE_URL}/users/me`, {
     method: 'GET',
     headers: {
+      "Access-Control-Allow-Origin": "*",
       'Content-Type': 'application/json',
       Authorization: `Bearer ${token}`,
     },
